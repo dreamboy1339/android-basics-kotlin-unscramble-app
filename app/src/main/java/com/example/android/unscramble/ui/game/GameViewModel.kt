3a274346c6728776b58238a8788b7425e1e228b5
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-06 23:31:06 +0900

    study: Change StateFlow and SavedStateHandler, fix init code

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -20,68 +20,109 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.TtsSpan
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import java.util.Calendar
+import kotlin.random.Random
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onSubscription
+import kotlinx.coroutines.flow.stateIn
+
+// TODO Fragment / Activity 인스턴스가 종료되어도 동일한 화면 내용을 유지 시키기.
+// TODO LiveData -> StateFlow
+// TODO SavedStateHandler 도입, 중요 데이터를 저장.
+// TODO 초기화 구문 개선
+
+class SaveableMutableStateFlow<T>(
+    private val savedStateHandle: SavedStateHandle,
+    private val key: String,
+    initialValue: T
+) {
+
+    private val state: StateFlow<T> = savedStateHandle.getStateFlow(key, initialValue)
+    var value: T
+        get() = state.value
+        set(value) {
+            savedStateHandle[key] = value
+        }
+
+    fun asStateFlow(): StateFlow<T> = state
+}
+
+fun <T> SavedStateHandle.getMutableStateFlow(
+    key: String,
+    initValue: T
+): SaveableMutableStateFlow<T> =
+    SaveableMutableStateFlow(this, key, initValue)
 
 /**
  * ViewModel containing the app data and methods to process the data
  */
-class GameViewModel : ViewModel() {
-    private val _score = MutableLiveData(0)
-    val score: LiveData<Int>
-        get() = _score
+class GameViewModel(private val stateHandler: SavedStateHandle) : ViewModel() {
+//    private val _score = MutableLiveData(0)
+//    val score: LiveData<Int>
+//        get() = _score
 
-    private val _currentWordCount = MutableLiveData(0)
-    val currentWordCount: LiveData<Int>
-        get() = _currentWordCount
+//    val score = stateHandler.getStateFlow("score", "")
+//    private fun setScore(value: Int) {
+//        stateHandler["score"] = value
+//    }
 
-    private val _currentScrambledWord = MutableLiveData<String>()
-    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
-        if (it == null) {
-            SpannableString("")
-        } else {
-            val scrambledWord = it.toString()
+    private val _score = stateHandler.getMutableStateFlow("score", 0)
+    val score: StateFlow<Int>
+        get() = _score.asStateFlow()
+
+    private val _currentWordCount = stateHandler.getMutableStateFlow("currentWordCount", 0)
+    val currentWordCount: StateFlow<Int>
+        get() = _currentWordCount.asStateFlow()
+
+    private val _currentScrambledWord = stateHandler.getMutableStateFlow("currentScrambledWord", "")
+    val currentScrambledWord: StateFlow<Spannable> = _currentScrambledWord
+        .asStateFlow()
+        .onSubscription {
+            if (currentWord.isEmpty()) nextWord()
+        }
+        .map { scrambledWord ->
             val spannable: Spannable = SpannableString(scrambledWord)
             spannable.setSpan(
-                    TtsSpan.VerbatimBuilder(scrambledWord).build(),
-                    0,
-                    scrambledWord.length,
-                    Spannable.SPAN_INCLUSIVE_INCLUSIVE
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
             )
             spannable
         }
-    }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), SpannableString(""))
+
 
     // List of words used in the game
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
-
-    init {
-        getNextWord()
-    }
-
-    /*
-     * Updates currentWord and currentScrambledWord with the next word.
-     */
-    private fun getNextWord() {
-        currentWord = allWordsList.random()
-        val tempWord = currentWord.toCharArray()
-        tempWord.shuffle()
-
-        while (String(tempWord).equals(currentWord, false)) {
-            tempWord.shuffle()
+//    private var wordsList: MutableList<String> = mutableListOf()
+    private var wordsList: List<String>
+        get() = stateHandler["wordsList"] ?: emptyList()
+        set(value) {
+            stateHandler["wordsList"] = value
         }
-        if (wordsList.contains(currentWord)) {
-            getNextWord()
-        } else {
+
+    private var currentWord: String
+        get() = stateHandler["currentWord"] ?: ""
+        set(value) {
+            val tempWord = value.toCharArray()
+            tempWord.shuffle()
+
+            do {
+                tempWord.shuffle()
+            } while (String(tempWord) == (value))
+
             Log.d("Unscramble", "currentWord= $currentWord")
             _currentScrambledWord.value = String(tempWord)
-            _currentWordCount.value = _currentWordCount.value?.inc()
-            wordsList.add(currentWord)
+            _currentWordCount.value += 1
+            wordsList = wordsList + currentWord
+
+            stateHandler["currentWord"] = value
         }
-    }
 
     /*
      * Re-initializes the game data to restart the game.
@@ -89,15 +130,15 @@ class GameViewModel : ViewModel() {
     fun reinitializeData() {
         _score.value = 0
         _currentWordCount.value = 0
-        wordsList.clear()
-        getNextWord()
+        wordsList = emptyList()
+        nextWord()
     }
 
     /*
     * Increases the game score if the player’s word is correct.
     */
     private fun increaseScore() {
-        _score.value = _score.value?.plus(SCORE_INCREASE)
+        _score.value += SCORE_INCREASE
     }
 
     /*
@@ -116,8 +157,12 @@ class GameViewModel : ViewModel() {
     * Returns true if the current word count is less than MAX_NO_OF_WORDS
     */
     fun nextWord(): Boolean {
-        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
-            getNextWord()
+        return if (_currentWordCount.value < MAX_NO_OF_WORDS) {
+            var nextWord: String
+            do {
+                nextWord = allWordsList.random(Random(Calendar.getInstance().timeInMillis))
+            } while (wordsList.contains(currentWord))
+            currentWord = nextWord
             true
         } else false
     }
